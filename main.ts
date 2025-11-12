4c3a64289e6a57ad52ed14ad1fb9ba0a24c24efd
default <rabira.motuma@stu.ocsb.ca>
2025-11-12 19:03:25 +0000

    added new main.ts file for calculating area of circle with variable and const values.

---
+++ main.ts
@@ -0,0 +1,24 @@
+/**
+ * @author Rabira Motuma
+ * @version 1.0.0
+ * @date 2025-11-11
+ * @fileoverview This file finds and displays the area of a circle with a radius of 5.6 cm.
+ */
+
+// number data type radius
+let radius: number = 5.6;
+
+// stores 3.14159 into PI
+const PI: number = 3.14159;
+
+// INPUT - none
+
+// PROCESS
+// calculate the area
+let answer: number = (PI * radius * radius);
+
+// OUTPUT
+// display the result
+console.log("The area of a circle with a radius of 5.6 cm is " + answer + " cm².");
+
+console.log("\nDone.");
